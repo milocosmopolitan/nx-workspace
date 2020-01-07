20ef5f24ac6614741b23e3a8c41e59e0c37d993e
Minho Kang <minhokang@minhos-mbp-2.home>
2020-01-07 12:46:10 -0500

    feat: set up swagger

--- apps/todo-api/src/main.ts
+++ apps/todo-api/src/main.ts
@@ -4,14 +4,29 @@
  */
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
+  /** Swagger */
+  const options = new DocumentBuilder()
+    .setTitle('Todo example')
+    .setDescription('The todo API description')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
+
+  
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
